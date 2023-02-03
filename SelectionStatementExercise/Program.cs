5f56182279a226744bc6f60c701a62fb0b29421e
David Benjamin <davidbenjamin0685@gmail.com>
2023-02-03 09:29:00 -0700

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,17 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(85);
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < 85)
+                Console.WriteLine("Too low playa!");
+            else if (userInput > 85)
+                Console.WriteLine("Too high playa!");
+            else
+                Console.WriteLine("You right on the money dawg!!");
         }
     }
 }
