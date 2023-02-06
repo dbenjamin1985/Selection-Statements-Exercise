0105308656eb6fcaab614766faa1ad1d5e40e9b5
David Benjamin <davidbenjamin0685@gmail.com>
2023-02-06 08:39:46 -0700

    lowered range for random number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,11 +5,11 @@
         static void Main()
         {
             var r = new Random();
-            var favNumber = r.Next(85);
+            var favNumber = r.Next(1, 10);
             var userInput = int.Parse(Console.ReadLine());
-            if (userInput < 85)
+            if (userInput < favNumber)
                 Console.WriteLine("Too low playa!");
-            else if (userInput > 85)
+            else if (userInput > favNumber)
                 Console.WriteLine("Too high playa!");
             else
                 Console.WriteLine("You right on the money dawg!!");
